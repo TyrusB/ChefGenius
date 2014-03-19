7ac52ddfcfb427b0a3eb736938bf2455c2cd416a
tyrusb <brooks85.ty@gmail.com>
2014-03-19 11:12:58 -0700

    Add associations and validations on existing models

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,2 +1,16 @@
+# == Schema Information
+#
+# Table name: ingredients
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)      not null
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Ingredient < ActiveRecord::Base
+  validates :name, :presence => true, :uniqueness => true
+
+  has_many :recipe_needs
+  has_many :recipes, :through => :recipe_id
 end
