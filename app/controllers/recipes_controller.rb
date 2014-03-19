8560e0e4abc0194819df4179afeddcbe4681fefd
Ty Brooks <brooks85.ty@gmail.com>
2014-03-19 15:48:14 -0700

    Edit form completed, needs to be modified still for add

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,16 +1,5 @@
 class RecipesController < ApplicationController
 
-  def index
-    @recipes = Recipe.all
-  end
-
-
-  def show
-    @recipe = Recipe.find(params[:id])
-    @needs = @recipe.recipe_needs
-    @steps = @recipe.steps
-  end
-
   def edit
     @recipe = Recipe.find(params[:id])
 
@@ -21,8 +10,28 @@ class RecipesController < ApplicationController
     2.times { @steps += [Step.new] }
   end
 
+  def index
+    @recipes = Recipe.all
+  end
+
   def new
-    @recipe = Recipe.new()
+    @recipe = Recipe.new
+
+    @needs = []
+    2.times { @needs << RecipeNeed.new }
+
+    @steps = []
+    2.times { @steps << Step.new }
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+    @needs = @recipe.recipe_needs
+    @steps = @recipe.steps
+  end
+
+  def update
+    fail
   end
 
 end
