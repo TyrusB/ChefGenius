32f2568c90ae0133541cfb0808d74677b983b356
Ty Brooks <brooks85.ty@gmail.com>
2014-03-19 11:12:58 -0700

    Add associations and validations on existing models

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,16 @@
+# == Schema Information
+#
+# Table name: recipes
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)      not null
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Recipe < ActiveRecord::Base
+  validates :title, presence: true, :uniqueness => true
+
+  has_many :recipe_needs, :dependent => :destroy
+  has_many :ingredients, :through => :recipe_needs
 end
--- app/models/recipe_need.rb
+++ app/models/recipe_need.rb
@@ -1,2 +1,18 @@
+# == Schema Information
+#
+# Table name: recipe_needs
+#
+#  id            :integer          not null, primary key
+#  recipe_id     :integer
+#  ingredient_id :integer
+#  amount        :float
+#  amount_type   :string(255)
+#  created_at    :datetime
+#  updated_at    :datetime
+#
+
 class RecipeNeed < ActiveRecord::Base
+  belongs_to :recipe
+  belongs_to :ingredient
+
 end
