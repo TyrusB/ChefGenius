6ad673f9588cc0f929cd4cc4471dc2f5fde1daae
Ty Brooks <brooks85.ty@gmail.com>
2014-04-15 23:52:18 -0700

    Add flash for contact page

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,7 +7,7 @@ ChefGenius::Application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { :host => "http://www.chef-genius.com" }
+  config.action_mailer.default_url_options = { :host => "smtp.sendgrid.net" }
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both thread web servers
